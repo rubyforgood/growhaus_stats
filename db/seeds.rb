95831c8677a7e2a3e368770370cb440f86a1dedb
Ben Morris <github@benmorris.net>
2015-07-31 15:58:24 -0400

    idempotentize seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-distribution = Department.create(name:"Distribution")
-production   = Department.create(name:"Production")
-education    = Department.create(name:"Education")
+distribution = Department.find_or_create_by(name:"Distribution")
+production   = Department.find_or_create_by(name:"Production")
+education    = Department.find_or_create_by(name:"Education")
 
-Form.create(name:"Food Boxes", google_form_url:"https://docs.google.com/forms/d/1dwTmarh5u5ZzJlIZYfwAb6XQH_TJDkyebVA_br23Kkg/viewform?embedded=true", frequency:'weekly', department:distribution)
+Form.find_or_create_by(name:"Food Boxes", google_form_url:"https://docs.google.com/forms/d/1dwTmarh5u5ZzJlIZYfwAb6XQH_TJDkyebVA_br23Kkg/viewform?embedded=true", frequency:'weekly', department:distribution)
