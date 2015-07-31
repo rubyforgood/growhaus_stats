9707cfb64b9a32e60cbf6d8aff6b5f377bc9f70d
Kevin <usacitizen80@hotmail.com>
2015-07-31 15:28:29 -0400

    adds schedule rb file for whenever gem

---
+++ config/schedule.rb
@@ -0,0 +1,20 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+every :monday, :at => '10am' do # Use any day of the week or :weekend, :weekday
+  runner "Task.do_something_great"
+end
+
+# Learn more: http://github.com/javan/whenever
