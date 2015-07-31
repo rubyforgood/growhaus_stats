71fe62b4eafcb9773551ac93deb7b09b881c3510
Austin Wood <i@austinwood.me>
2015-07-31 14:57:11 -0400

    Allow user to logout if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "home#show"
   get "/profile", to: "profiles#show"
   get "/auth/:provider/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
