1448d0245345aee94d20232a9873a5792190f706
Kevin <usacitizen80@hotmail.com>
2015-07-31 14:20:13 -0400

    adds three email reminders

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,14 +4,21 @@ class NotificationMailer < ApplicationMailer
    
   	emails = User.all.map(&:email)
 
-    mail to: emails
+    mail to: emails, :from => "info@growhaus.com", :subject => "Weekly Data Submission Reminder"
   end
 
   def not_submitted_reminder #occurs on Tuesday at noon
    
-  	emails = 
+  	emails = User.all.map(&:email)
 
-    mail to: emails
+    mail to: emails, :from => "info@growhaus.com", :subject => "Weekly Data Submission Reminder, Part 2"
   end
+
+  def submissions_complete #occurs on Tuesday at noon
+   
+  	emails = User.find_by(admin: true)
+
+    mail to: emails, :from => "info@growhaus.com", :subject => "Data Submission Complete"
+  end	  
 
 end
