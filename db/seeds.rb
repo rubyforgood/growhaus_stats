030550a6740b7b8e71c1902e36e288aa0b0ef62a
Ben Morris <github@benmorris.net>
2015-07-31 15:34:22 -0400

    integrate department model into form, seed all 3 departments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Form.create(name:"Food Boxes", google_form_url:"https://docs.google.com/forms/d/1dwTmarh5u5ZzJlIZYfwAb6XQH_TJDkyebVA_br23Kkg/viewform?embedded=true", frequency:'weekly')
+distribution = Department.create(name:"Distribution")
+production   = Department.create(name:"Production")
+education    = Department.create(name:"Education")
+
+Form.create(name:"Food Boxes", google_form_url:"https://docs.google.com/forms/d/1dwTmarh5u5ZzJlIZYfwAb6XQH_TJDkyebVA_br23Kkg/viewform?embedded=true", frequency:'weekly', department:distribution)
