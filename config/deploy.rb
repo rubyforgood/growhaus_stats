abc45bababe6609c262140d0bfd8daaf77bec233
Brandon Rice <brice84@gmail.com>
2015-07-31 17:59:46 -0400

    Add restart command to capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,10 +39,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      execute "cd #{current_path} bundle exec pumactl phased-restart"
     end
   end
 
