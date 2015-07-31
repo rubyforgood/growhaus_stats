1d8f9e78d46c400e5dc03cebc2a9d68c361813a5
Austin Wood <i@austinwood.me>
2015-07-31 14:44:02 -0400

    Store user data when they login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_or_create_from_auth(request.env["omniauth.auth"])
+    if user
+      session[:user_id] = user.id
+      redirect_to profile_path
+    else
+      redirect_to root_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+
+    user.name = data.info.name
+    user.email = data.info.email
+    user.image_url = data.info.image
+    user.account_url = data.info.urls.Google
+    user.provider = data.provider
+    user.token = data.credentials.token
+    user.uid = data.uid
+    user.save
+
+    user
+  end
 end
