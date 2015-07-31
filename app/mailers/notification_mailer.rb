f60aad0029e4e67c40b72e0429170c1aa9f3ecce
Kevin <usacitizen80@hotmail.com>
2015-07-31 16:11:59 -0400

    completes the rake task, mailer and view for the first notification email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,26 +1,26 @@
 class NotificationMailer < ApplicationMailer
 
-  def weekly_submission_reminder #occurs on Monday
-   # Date::DAYNAMES[Date.today.wday] == "Monday"
-  	@users = User.all #.find_by(admin: true)
-  	emails = users.all.map(&:email)
-
-    mail(to: emails, :subject => "Weekly Data Submission Reminder")
-  end
-
-  def not_submitted_reminder #occurs on Tuesday at noon
-    
-    @users = User.all #.find_by(admin: true)
-  	emails = users.all.map(&:email)
-
-    mail(to: emails, :subject => "Weekly Data Submission Reminder, Part 2")
-  end
-
-  def submissions_complete #occurs on Tuesday at noon
+  def weekly_submission_reminder(user, url) #occurs on Monday at 10am
    
-  	emails = User #.find_by(admin: true)
+  	@user = user
+    @url = url
 
-    mail(to: emails, :subject => "Data Submission Complete")
+    mail(to: user.email, :subject => "Weekly Data Submission Reminder")
+  end
+
+  def not_submitted_reminder(user, url) #occurs on Tuesday at noon
+  
+    @user = user
+    @url = url
+
+    mail(to: user.email, :subject => "Weekly Data Submission Reminder, Part 2")
+  end
+
+  def submissions_complete(user) #occurs on Wednesday
+   
+  	@user = user
+
+    mail(to: user.email, :subject => "Data Submission Complete")
   end	  
 
 end
