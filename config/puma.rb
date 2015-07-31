395d13c3cf8f8e811d60ded0c65e1b360fe77a07
Brandon Rice <brice84@gmail.com>
2015-07-31 16:19:15 -0400

    Update puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -5,21 +5,20 @@ workers 1
 threads 1, 6
 
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
 
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/tmp/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
