cdadc8f460828ed195b8bccb90cbecb7e1fe56c5
Kevin <usacitizen80@hotmail.com>
2015-07-31 12:37:49 -0400

    creates notification mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,17 @@
 class NotificationMailer < ApplicationMailer
+
+  def weekly_submission_reminder #occurs on Monday
+   
+  	emails = User.all.map(&:email)
+
+    mail to: emails
+  end
+
+  def not_submitted_reminder #occurs on Tuesday at noon
+   
+  	emails = 
+
+    mail to: emails
+  end
+
 end
