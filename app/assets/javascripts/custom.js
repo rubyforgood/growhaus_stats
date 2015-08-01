1d715eb30b50754f7b7b0444e5091daa351971fb
Ben Morris <github@benmorris.net>
2015-08-01 11:47:15 -0400

    implement slide out nav

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+    menuWidth: 300
+  });
+  $("#close-side-nav").click(function(){
+    $('.button-collapse').sideNav('hide');
+  })
 });
