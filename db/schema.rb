790c0f0f215f1012cdb4275966e22f23d5988de0
Brandon Rice <brice84@gmail.com>
2015-08-01 16:01:54 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801183808) do
+ActiveRecord::Schema.define(version: 20150801192310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150801183808) do
     t.integer  "department_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
     t.string   "google_sheet_url"
   end
 
