3b86cdd6d3048a5ec55862a57a29838288e92a68
Brandon Rice <brice84@gmail.com>
2015-08-01 16:25:06 -0400

    Comments changes.

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -2,14 +2,15 @@
 #
 # Table name: forms
 #
-#  id              :integer          not null, primary key
-#  name            :string
-#  google_form_url :string
-#  frequency       :string
-#  department_id   :integer
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  user_id         :integer
+#  id               :integer          not null, primary key
+#  name             :string
+#  google_form_url  :string
+#  frequency        :string
+#  department_id    :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :integer
+#  google_sheet_url :string
 #
 
 require 'rails_helper'
