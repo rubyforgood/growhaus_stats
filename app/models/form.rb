c1bbdc24841f6f43359e831480f5eec548d2c9be
Kevin <usacitizen80@hotmail.com>
2015-08-01 16:12:36 -0400

    fixes an error that would make all valid adresses fail validation

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,6 @@
 class Form < ActiveRecord::Base
   belongs_to :department
-  validates :google_form_url, format: { with: /https:\/\/google.docs.com\/forms.*/, message: 'must start with "https://google.docs.com/forms..."'  }
+  validates :google_form_url, format: { with: /https:\/\/docs.google.com\/forms.*/, message: 'must start with "https://google.docs.com/forms..."'  }
+  validates :google_sheet_url, format: { with: /https:\/\/docs.google.com\/spreadsheet.*/, message: 'must start with "https://google.docs.com/spreadsheet..."'  }
 
 end
