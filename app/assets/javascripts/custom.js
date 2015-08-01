4acf79a0192adaf1a13645d3f58a97483608585f
Ben Morris <github@benmorris.net>
2015-08-01 12:07:37 -0400

    add dual js loading for page load and doc.ready

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,16 @@
-$( document ).ready(function() {
+setupNav = function(){
   $(".button-collapse").sideNav({
     menuWidth: 300
   });
   $("#close-side-nav").click(function(){
     $('.button-collapse').sideNav('hide');
   })
+};
+
+$( document ).ready(function() {
+  setupNav();
+});
+
+$(document).on('page:load', function () {
+  setupNav();
 });
