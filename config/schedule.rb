41699919126c5c3a3656f8fcda3b59ebf4fd097a
Kevin <usacitizen80@hotmail.com>
2015-08-02 10:47:42 -0400

    comments out the wednesday completion email

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,28 +1,17 @@
 # Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
+# weekly reminder email
 every :monday, :at => '10am' do # Use any day of the week or :weekend, :weekday
   rake "weekly_submission_reminder"
 end
 
+# second reminder email, used if it was possible to identify unfilled forms and their owners
 # every :tuesday, :at => '11am' do # Use any day of the week or :weekend, :weekday
 #   rake "not_submitted_reminder"
 # end
 
-every :wednesday, :at => '12pm' do # Use any day of the week or :weekend, :weekday
-  rake "submissions_complete"
-end
+# completion email to be sent to the superusers/admin
+#every :wednesday, :at => '12pm' do
+#  rake "submissions_complete"
+#end
 
-# Learn more: http://github.com/javan/whenever
