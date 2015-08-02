3709817c00845cd741ddc1305cfdf8fce6757974
Brandon Rice <brice84@gmail.com>
2015-08-02 02:29:58 -0400

    Handle omniauth failure.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,39 +3,41 @@ class DashboardController < ApplicationController
   def show
     @departments = Department.all
 
-    revenue_forms = @departments.map do |dept|
-      [dept.name, Form.where(department: dept).with_revenue]
-    end
-
-    revenue_forms = Hash[revenue_forms]
-    revenue       = revenue_forms.map { |i, j| j.first.revenue }
-    columns       = ['Date', @departments.map(&:name)].flatten
-    num_dates     = revenue.first.count
-    
-    @revenue_chart_data = []
-    @revenue_chart_data << columns
-
-    num_dates.times do |index|
-      date = revenue.find do |dept|
-        dept[index].length > 0
+    begin
+      revenue_forms = @departments.map do |dept|
+        [dept.name, Form.where(department: dept).with_revenue]
       end
 
-      date = date[index].first
+      revenue_forms = Hash[revenue_forms]
+      revenue       = revenue_forms.map { |i, j| j.first.revenue }
+      columns       = ['Date', @departments.map(&:name)].flatten
+      num_dates     = revenue.first.count
+      
+      @revenue_chart_data = []
+      @revenue_chart_data << columns
 
-      amounts = revenue.map do |dept|
-        dept[index].last.to_i
+      num_dates.times do |index|
+        date = revenue.find do |dept|
+          dept[index].length > 0
+        end
+
+        date = date[index].first
+
+        amounts = revenue.map do |dept|
+          dept[index].last.to_i
+        end
+
+        @revenue_chart_data << [date, amounts].flatten
       end
-
-      @revenue_chart_data << [date, amounts].flatten
+    rescue
+      @revenue_chart_data = [
+        ['Month', 'Distribution', 'Production', 'Education'],
+        ['May', 1030, 540, 350],
+        ['June', 660, 1120, 300],
+        ['July', 1170, 460, 250],
+        ['August', 1000, 400, 200]
+      ]
     end
-
-    #@revenue_chart_data = [
-    #  ['Month', 'Distribution', 'Production', 'Education'],
-    #  ['May', 1030, 540, 350],
-    #  ['June', 660, 1120, 300],
-    #  ['July', 1170, 460, 250],
-    #  ['August', 1000, 400, 200]
-    #]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/profile", to: "profiles#show"
   get "/dashboard", to: "dashboard#show"
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
+  get '/auth/failure', to: 'home#show'
   match "/logout", to: "sessions#destroy", via: [:get, :delete]
 end
