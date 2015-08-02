b203fbcc74acd41691548c4bae61e2f6ce65ece4
Kevin <usacitizen80@hotmail.com>
2015-08-02 11:37:33 -0400

    makes the links in emails to the root url

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,6 +3,7 @@ class NotificationMailer < ApplicationMailer
   def weekly_submission_reminder(user) #occurs on Monday at 10am
 
     @user = user
+    @url = root_url.to_s
 
     mail(to: user.email, :subject => "Weekly Data Submission Reminder")
   end
@@ -13,13 +14,14 @@ class NotificationMailer < ApplicationMailer
     @forms = forms
     @user = user
 
+
     mail(to: user.email, :subject => "Weekly Data Submissions Not Complete")
   end
 
   def submissions_complete(user) #occurs on Wednesday
 
     @user = user
-
+    @url = root_url.to_s
     mail(to: user.email, :subject => "Weekly Data Submission Complete")
   end
 
